81c2f4d8f8cc137460eea947296aca950288aaf0
Felippi <109880105+WhiteAngel0604@users.noreply.github.com>
2026-08-12 18:07:48 -0300

    Update Conexao.java
    Descobri que essa bosta é tudo no msm arquivo

--- Conexao.java
+++ Conexao.java
@@ -3,6 +3,7 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
  */
 package conexao;
+package controle;
 
 import javax.swing.JOptionPane;
 import java.sql.*;
@@ -58,7 +59,43 @@ public class Conexao {
             JOptionPane.showMessageDialog(null,"Erro no comando SQL! \n Erro: "+excecao,"Mensagem do Programa",JOptionPane.INFORMATION_MESSAGE);
         }
     }
-    
+    public class frmTela extends javax.swing.JFrame {
+
+    // Construtor da Tela
+    public frmTela() {
+        initComponents();
+        con_cliente = new Conexao();
+        con_cliente.conecta();
+        con_cliente.executaSQL("select * from tbclientes order by cod");
+        preencherTabela();
+    } 
+
+    // Método preencherTabela (AGORA FORA DO CONSTRUTOR)
+    public void preencherTabela() {
+        tblClientes.getColumnModel().getColumn(0).setPreferredWidth(4);
+        tblClientes.getColumnModel().getColumn(1).setPreferredWidth(150);
+        tblClientes.getColumnModel().getColumn(2).setPreferredWidth(11);
+        tblClientes.getColumnModel().getColumn(3).setPreferredWidth(14);
+        tblClientes.getColumnModel().getColumn(4).setPreferredWidth(100);
+
+        DefaultTableModel modelo = (DefaultTableModel) tblClientes.getModel();
+        modelo.setNumRows(0);
+
+        try {
+            con_cliente.resultset.beforeFirst();
+            while (con_cliente.resultset.next()) {
+                modelo.addRow(new Object[]{
+                    con_cliente.resultset.getString("cod"), 
+                    con_cliente.resultset.getString("nome") // Complete o restante dos campos aqui
+                });
+            }
+        } catch (SQLException erro) {
+            JOptionPane.showMessageDialog(null, "\n Erro ao listar dados da tabela!! :\n" + erro);
+        }
+    }
+public static void main(String args[]) {
+}
+} // <-- Esta chave fecha a classe frmTela por completo
        
     
     
